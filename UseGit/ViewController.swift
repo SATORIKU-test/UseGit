5c7d8a1c9d638f4649321fa4dc3e8f6db6dd5ae3
SATORIKU-test <rikuto.sato.share@gmail.com>
2020-09-10 15:54:29 +0900

    first commit

---
+++ UseGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UseGit
+//
+//  Created by Sub on 2020/09/10.
+//  Copyright © 2020 Sub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
